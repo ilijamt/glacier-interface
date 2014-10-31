dad2291a89fa2b0e093c1996d3cd887e535e88b5
Ilija Matoski <ilijamt@gmail.com>
2014-10-31 21:27:30 +0100

    Code format

--- src/main/java/com/matoski/glacier/enums/CliCommands.java
+++ src/main/java/com/matoski/glacier/enums/CliCommands.java
@@ -2,14 +2,23 @@ package com.matoski.glacier.enums;
 
 public enum CliCommands {
 
-    Help("help"), ListVaults("list-vaults"), CreateVault("create-vault"), DeleteVault(
-	    "delete-vault"), ListVaultJobs("list-vault-jobs"), VaultJobInfo(
-	    "vault-job-info"), InventoryRetrieve("inventory-retrieve"), InventoryDownload(
-	    "inventory-download"), ListJournal("list-journal"), DownloadArchive(
-	    "download-archive"), DeleteArchive("delete-archive"), UploadArchive(
-	    "upload-archive"), ListMultipartUploads("list-multipart-uploads"), MultipartUploadInfo(
-	    "multipart-upload-info"), AbortMultipartUpload(
-	    "abort-multipart-upload");
+    Help("help"),
+    ListVaults("list-vaults"),
+    CreateVault("create-vault"),
+    DeleteVault("delete-vault"),
+    ListVaultJobs("list-vault-jobs"),
+    VaultJobInfo("vault-job-info"),
+    InventoryRetrieve("inventory-retrieve"),
+    InventoryDownload("inventory-download"),
+    ListJournal("list-journal"),
+    DownloadArchive("download-archive"),
+    DeleteArchive("delete-archive"),
+    UploadArchive("upload-archive"),
+    ListMultipartUploads("list-multipart-uploads"),
+    MultipartUploadInfo("multipart-upload-info"),
+    AbortMultipartUpload("abort-multipart-upload"),
+    Sync("sync"),
+    Purge("purge");
 
     public static CliCommands from(String x) {
 	for (CliCommands currentType : CliCommands.values()) {
