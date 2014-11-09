9961702683113c7675c9640b3f7e62b99c176ead
Ilija Matoski <ilijamt@gmail.com>
2014-11-09 20:37:58 +0100

    Created package task for debian in gradle

--- package.sh
+++ package.sh
@@ -3,8 +3,20 @@
 SCRIPT=`realpath -s $0`
 WORK_DIR=`dirname $SCRIPT`
 TMP_DIR=`mktemp -d`
+DEB_FILE="${TMP_DIR}.deb"
 
-if [ ! -f $WORK_DIR/build/libs/glacier-interface.jar ]; then
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 2 ] || die "2 arguments required, $# provided, $0 <version> <pkg-name>"
+
+PKG_VERSION="$1"
+PKG_NAME="$2"
+DEST_DEB_FILE="${WORK_DIR}/build/${PKG_NAME}-${PKG_VERSION}.deb"
+
+if [ ! -f "$WORK_DIR/build/libs/$PKG_NAME.jar" ]; then
 	echo "Cannot find main jar file"
 	exit -1;
 fi;
@@ -13,16 +25,41 @@ fi;
 cp -R $WORK_DIR/debian $TMP_DIR/DEBIAN
 
 # Copy the built files to the file
-mkdir -p $TMP_DIR/usr/share/glacier-interface
-mkdir -p $TMP_DIR/usr/bin
-cp $WORK_DIR/build/libs/glacier-interface.jar $TMP_DIR/usr/share/glacier-interface
-cp $WORK_DIR/gi $TMP_DIR/usr/bin
+mkdir -p "$TMP_DIR/usr/share/$PKG_NAME"
+mkdir -p "$TMP_DIR/usr/share/doc/$PKG_NAME"
+mkdir -p "$TMP_DIR/usr/bin"
+cp "$WORK_DIR/build/libs/$PKG_NAME.jar" "$TMP_DIR/usr/share/$PKG_NAME"
+cp "$WORK_DIR/gi" "$TMP_DIR/usr/bin"
+gzip -9c "${WORK_DIR}/debian/changelog" > "${TMP_DIR}/usr/share/doc/${PKG_NAME}/changelog.gz"
+cp "${WORK_DIR}/debian/copyright" "${TMP_DIR}/usr/share/doc/${PKG_NAME}/copyright"
+chmod +x "$TMP_DIR/usr/bin/gi"
+rm "$TMP_DIR/DEBIAN/changelog" "$TMP_DIR/DEBIAN/compat" "$TMP_DIR/DEBIAN/copyright"
+
+touch "$TMP_DIR/DEBIAN/md5sums"
+cd $TMP_DIR
+md5sum "usr/share/$PKG_NAME/$PKG_NAME.jar" >> "$TMP_DIR/DEBIAN/md5sums"
+md5sum "usr/share/doc/${PKG_NAME}/changelog.gz" >> "$TMP_DIR/DEBIAN/md5sums"
+md5sum "usr/share/doc/${PKG_NAME}/copyright" >> "$TMP_DIR/DEBIAN/md5sums"
+md5sum "usr/bin/gi" >> "$TMP_DIR/DEBIAN/md5sums"
+
+PACKAGE_SIZE=`du -bs "$TMP_DIR" | cut -f 1`
+PACKAGE_SIZE=$((PACKAGE_SIZE/1024))
+echo "Installed-Size: $PACKAGE_SIZE" >> "$TMP_DIR/DEBIAN/control"
+
+fakeroot chown -R root "$TMP_DIR"
+fakeroot chgrp -R root "$TMP_DIR"
 
 # Package 
 cd $TMP_DIR
 fakeroot dpkg-deb --build $TMP_DIR
 
 # Clean 
-#rm -rf $TMP_DIR
+rm -rf $TMP_DIR
+
+mv "$DEB_FILE" "$DEST_DEB_FILE"
+
+lintian -i "$DEST_DEB_FILE" > "$WORK_DIR/build/lint" 
+
+rm $TMP_DIR -rf
 
 exit 0
