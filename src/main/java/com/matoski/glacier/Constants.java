35f58024b258251032eb108c2c0e593f5fd285b9
Ilija Matoski <ilijamt@gmail.com>
2014-12-15 00:28:19 +0100

    Bump up version

--- src/main/java/com/matoski/glacier/Constants.java
+++ src/main/java/com/matoski/glacier/Constants.java
@@ -9,7 +9,7 @@ package com.matoski.glacier;
 public class Constants {
 
   /** Application version. */
-  public static final String VERSION = "0.2";
+  public static final String VERSION = "0.3.4";
 
   /** Human readable digits, used when displaying data. */
   public static final Integer DEFAULT_HUMAN_READABLE_DIGITS = 2;
